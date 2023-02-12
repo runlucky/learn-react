2eb792f3ea78f7cfb5d0d499e5922df4068260c5
Kakeru Fukuda <stand.by.we@gmail.com>
2023-02-12 15:39:49 +0900

    onCallback

--- src/components/Hooks/CallbackCounter.tsx
+++ src/components/Hooks/CallbackCounter.tsx
@@ -1,15 +1,39 @@
-import { useState, useCallback } from "react"
+import { useState, useCallback, memo } from "react"
 
 export const CallbackCounter = () => {
     const [value, setValue] = useState(0)
 
     const increment = () => setValue(value + 1)
     const decrement = () => setValue(value - 1)
+
+    // memo化したcallbackにラップすることで再描画を抑制する
     const double = useCallback(() => setValue(value * 2), [])
 
     return <div>
         <p>Callback Counter: {value}</p>
-        
+        <DecrementButton onClick={decrement} />
+        <IncrementButton onClick={increment} />
+        <DoubleButton onClick={double} />
     </div>    
 
 }
+
+type ButtonProps = {
+    onClick: () => void
+}
+
+const DecrementButton = (props: ButtonProps) => {
+    console.log("Decrement 再描画")
+    return <button onClick={props.onClick}>-</button>
+}
+
+// ただmemo化しただけでは再描画されてしまう
+const IncrementButton = memo((props: ButtonProps) => {
+    console.log("Increment 再描画")
+    return <button onClick={props.onClick}>+</button>
+})
+
+const DoubleButton = memo((props: ButtonProps) => {
+    console.log("Double 再描画")
+    return <button onClick={props.onClick}>x2</button>
+})
